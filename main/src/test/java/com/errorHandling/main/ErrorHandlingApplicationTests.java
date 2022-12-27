2f1d1315aa20d5a67caa51bbe9c7a56d91f7779b
visels <elviskipchumba@yahoo.com>
2022-12-27 15:29:33 +0300

    project setup

---
+++ main/src/test/java/com/errorHandling/main/ErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorHandling.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
