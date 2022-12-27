7739bf14a9b6713abf7bbe2364ec064f5791b62d
visels <elviskipchumba@yahoo.com>
2022-12-27 23:57:21 +0300

    added customer model

---
+++ main/src/main/java/com/errorHandling/main/Customer/Customer.java
@@ -0,0 +1,19 @@
+package com.errorHandling.main.Customer;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+}
