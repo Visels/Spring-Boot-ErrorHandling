3748c75a1c00a89fb9b24049b6ef1b86d44d40c5
visels <elviskipchumba@yahoo.com>
2022-12-30 23:23:38 +0300

    no such element exception handled

--- main/src/main/java/com/errorHandling/main/Controller/Errors/RestExceptionHandler.java
+++ main/src/main/java/com/errorHandling/main/Controller/Errors/RestExceptionHandler.java
@@ -2,10 +2,27 @@ package com.errorHandling.main.Controller.Errors;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
 
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleNoSuchElementException(HttpServletRequest req, NoSuchElementException ex){
+
+
+        return null;
+    }
+
+
 
 }
