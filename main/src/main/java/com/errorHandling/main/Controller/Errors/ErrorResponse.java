6b7cdeb26992dbc302407e721f58743cd129caf0
visels <elviskipchumba@yahoo.com>
2022-12-30 23:32:04 +0300

    added error response

---
+++ main/src/main/java/com/errorHandling/main/Controller/Errors/ErrorResponse.java
@@ -0,0 +1,57 @@
+package com.errorHandling.main.Controller.Errors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+
+    private HttpStatus status;
+
+    @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "yyyyMMdd hh:mm:ss")
+    private LocalDateTime timeStamp;
+    private String message;
+
+
+    public ErrorResponse(HttpStatus status, LocalDateTime timeStamp, String message) {
+        this.status = status;
+        this.timeStamp = timeStamp;
+        this.message = message;
+    }
+
+
+    public ErrorResponse(HttpStatus status){
+        this.status = status;
+    }
+
+    public ErrorResponse(){
+        timeStamp = LocalDateTime.now();
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
