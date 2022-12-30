fdeab6dcf4852a8ae50129e29c6a791e9c80eada
visels <elviskipchumba@yahoo.com>
2022-12-30 23:25:32 +0300

    sql integrity error handled

--- main/src/main/java/com/errorHandling/main/Controller/Errors/RestExceptionHandler.java
+++ main/src/main/java/com/errorHandling/main/Controller/Errors/RestExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.NoSuchElementException;
 
 
@@ -18,6 +19,13 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleNoSuchElementException(HttpServletRequest req, NoSuchElementException ex){
+
+
+        return null;
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<Object> handleSqlIntegrityConstraintViolationException(HttpServletRequest req, SQLIntegrityConstraintViolationException ex){
 
 
         return null;
