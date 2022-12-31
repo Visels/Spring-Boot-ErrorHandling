7bc00e8c12764e9d2e35097bffc1f16fa7ae2bdb
visels <elviskipchumba@yahoo.com>
2022-12-31 23:10:51 +0300

    added bcrypt password encoder

--- main/src/main/java/com/errorHandling/main/Security/PasswordEncoder.java
+++ main/src/main/java/com/errorHandling/main/Security/PasswordEncoder.java
@@ -1,9 +1,22 @@
 package com.errorHandling.main.Security;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class PasswordEncoder {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public PasswordEncoder(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     public String encode(String password){
-//        TODO: implement encryption
-        return password;
+
+        return bCryptPasswordEncoder.encode(password);
     }
 }
