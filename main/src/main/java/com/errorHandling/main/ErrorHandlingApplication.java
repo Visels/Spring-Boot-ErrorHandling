f3148ce32c4d0a2f61ae91ea05490c29dc08c279
visels <elviskipchumba@yahoo.com>
2022-12-31 23:21:22 +0300

    disabled spring security autoconfiguration

--- main/src/main/java/com/errorHandling/main/ErrorHandlingApplication.java
+++ main/src/main/java/com/errorHandling/main/ErrorHandlingApplication.java
@@ -3,8 +3,9 @@ package com.errorHandling.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class ErrorHandlingApplication {
 
 	public static void main(String[] args) {
