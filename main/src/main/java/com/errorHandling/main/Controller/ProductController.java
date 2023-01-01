85213119e4f40df77d15c02282febecd416fb61a
visels <elviskipchumba@yahoo.com>
2023-01-02 00:04:41 +0300

    sorted controller error

--- main/src/main/java/com/errorHandling/main/Controller/ProductController.java
+++ main/src/main/java/com/errorHandling/main/Controller/ProductController.java
@@ -36,8 +36,8 @@ public class ProductController {
         return ResponseHandler.responseBuilder(
                 "product successfully added",
                 HttpStatus.OK,
-                productService.
-        )
+                productService.addProduct(product)
+        );
     }
 
 
