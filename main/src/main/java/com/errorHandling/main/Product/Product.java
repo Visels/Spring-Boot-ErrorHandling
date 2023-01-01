a415169eeb57b8ca3e409a5011701ee142739392
visels <elviskipchumba@yahoo.com>
2023-01-01 23:46:05 +0300

    added products class

---
+++ main/src/main/java/com/errorHandling/main/Product/Product.java
@@ -0,0 +1,17 @@
+package com.errorHandling.main.Product;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products_table")
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private double amount;
+    private String imageURL;
+}
